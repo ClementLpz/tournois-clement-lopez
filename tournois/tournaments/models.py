72e7a0602b372616021a2dc9f836c5f35cf9d7e5
Clement Lopez <clement.lopez@student.isae-supaero.fr>
2023-04-17 15:12:49 +0200

    Add tournament model and make DB structure

--- tournois/tournaments/models.py
+++ tournois/tournaments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Tournoi(models.Model):
+    """
+    A tournament, with several pools, and teams
+    """
+    name = models.CharField(max_length=200)
+    place = models.CharField(max_length=200, null=True)
+    date = models.CharField(max_length=200, null=True)
+    N_pools = models.IntegerField('Number of pools')
+    N_teams_per_pools = models.IntegerField('Number of teams per pools')
+
+    def __str__(self):
+        return self.name
