162893a5a2eb37b7a5ca0714180acef154c2094f
Clement Lopez <clement.lopez@student.isae-supaero.fr>
2023-04-20 19:20:09 +0200

    Add Bootstrap style to the comment post & editing forms

--- tournois/tournaments/forms.py
+++ tournois/tournaments/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Comment
 
 class CommentForm(forms.ModelForm):
-    message = forms.CharField(widget=forms.Textarea(attrs={'cols': 15, 'rows': 1}), label='')
+    message = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 1}), label='')
     class Meta:
         model = Comment
         fields = ['message']
