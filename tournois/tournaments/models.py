c13afbb92ec4c3c12d567da8dcd766f2e13539b9
Raphael Hassine <raphael.hassine@student.isae-supaero.fr>
2023-05-03 15:51:05 +0200

    bug fix and requirements update

--- tournois/tournaments/models.py
+++ tournois/tournaments/models.py
@@ -135,6 +135,7 @@ class Match(models.Model):
     team2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team2_set')
     score2 = models.IntegerField(default=0)
     pool = models.ForeignKey(Pool, on_delete=models.CASCADE, null=True)
+    round = models.IntegerField(default=1)
     
 
     def __str__(self):
