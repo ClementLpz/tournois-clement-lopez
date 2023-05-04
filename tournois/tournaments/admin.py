07898e52adcb05cb1b3adac540841c19e9855bd7
Raphael Hassine <raphael.hassine@student.isae-supaero.fr>
2023-05-04 13:42:46 +0200

    fix pool points conceded and scored during team creation

--- tournois/tournaments/admin.py
+++ tournois/tournaments/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 
 from .models import Tournament, Pool, Team, Match, Comment, Place
+class TeamAdmin(admin.ModelAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if not obj:  # If this is a new object (not yet created)
+            form.base_fields.pop('pool_points', None)
+            form.base_fields.pop('conceded', None)
+            form.base_fields.pop('scored', None)
+        return form
 
 admin.site.register(Tournament)
 admin.site.register(Pool)
-admin.site.register(Team)
+admin.site.register(Team, TeamAdmin)
 admin.site.register(Match)
 admin.site.register(Comment)
 admin.site.register(Place)
