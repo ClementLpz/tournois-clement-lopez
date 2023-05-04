d5a8e814f995dda029213ad6b9e3ce2a7db12b7c
Raphael Hassine <raphael.hassine@student.isae-supaero.fr>
2023-05-04 14:39:25 +0200

    removing dead code

--- tournois/tournois/settings.py
+++ tournois/tournois/settings.py
@@ -44,8 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    
+    'django.contrib.staticfiles', 
     "bootstrap5",
     
     
