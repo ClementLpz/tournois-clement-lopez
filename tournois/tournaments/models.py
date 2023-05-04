1fef9c30fc2c042436fdde9848aa632ee1eac1f1
Raphael Hassine <raphael.hassine@student.isae-supaero.fr>
2023-05-04 11:52:22 +0200

    Fixing compute all matches null scores

--- tournois/tournaments/models.py
+++ tournois/tournaments/models.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import random
 from django.db import models
 from django.dispatch import receiver
@@ -71,12 +72,16 @@ class Pool(models.Model):
         list_all_matches = list(self.teams.all())
         for i in range(n - 1):
             for j in range(i + 1, n):
-                match = Match(team1 = list_all_matches[i], 
-                              team2 = list_all_matches[j], 
-                              pool = self, 
-                              date = self.tournament.date,  
-                              hour = "10h - 12h", 
-                              place = self.tournament.place)
+                random_score1 = random.randint(0, 7)
+                random_score2 = random.randint(0, 7)
+                match = Match(team1=list_all_matches[i],
+                            team2=list_all_matches[j],
+                            pool=self,
+                            date=self.tournament.date,
+                            hour="10h - 12h",
+                            place=self.tournament.place,
+                            score1=random_score1,
+                            score2=random_score2)
                 match.save()
 
     """
@@ -86,36 +91,37 @@ class Pool(models.Model):
     :returns: a list with the teams ranked by pool points
     """
 
-    def compute_ranking(self):
+    def compute_ranking(self): 
         
         teams = self.teams.all()
-        computed_pool_points = {}
+        computed_pool_points = defaultdict(int)
         ranked_computed_teams = []
 
-        for team in teams :
+        for team in teams:
+            team.pool_points = 0
 
-            for match in Match.objects.filter(pool=self) :
-                match.team1.scored=0
-                match.team1.conceded=0
-                match.team2.scored=0
-                match.team2.conceded=0
+            for match in Match.objects.filter(pool=self):
+                match.team1.scored = 0
+                match.team1.conceded = 0
+                match.team2.scored = 0
+                match.team2.conceded = 0
 
-                if match.team1 == team :
+                if match.team1 == team:
                     team.scored += match.score1
                     team.conceded += match.score2
 
-                    if (match.score1 == match.score2) :
+                    if match.score1 == match.score2:
                         team.pool_points += 1
-                    elif (match.score1 > match.score2) :
+                    elif match.score1 > match.score2:
                         team.pool_points += 3
 
-                if match.team2 == team :
+                if match.team2 == team:
                     team.scored += match.score2
                     team.conceded += match.score1
 
-                    if (match.score1 == match.score2) :
+                    if match.score1 == match.score2:
                         team.pool_points += 1
-                    elif (match.score2 > match.score1) :
+                    elif match.score2 > match.score1:
                         team.pool_points += 3
 
             computed_pool_points[team] = team.pool_points
